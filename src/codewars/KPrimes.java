b6b082b7ca8c96d4b2d8db12e2e4fc17e203df00
Little-Captain <coder.littlecaptain@gmail.com>
2019-05-28 12:35:27 +0800

    KPrimes完成了，但是提交后，执行超时，需要优化

--- src/codewars/KPrimes.java
+++ src/codewars/KPrimes.java
@@ -1,7 +1,6 @@
 package codewars;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.LongStream;
 
@@ -9,25 +8,30 @@ import java.util.stream.LongStream;
 public class KPrimes {
 
     public static long[] countKprimes(int k, long start, long end) {
-        final long[] primes = primes(0, end);
-        LongStream
-                .range(start, end + 1)
-                .boxed()
-                .map(n -> {
-                    List<Long> factors = new ArrayList<>();
-                    KPrimes.primeFactors(n, primes, factors);
-                    return factors;
-                })
-                .filter(factors -> factors.size() == k)
-                .forEach(factors -> {
-                    System.out.println("----------------");
-                    factors.forEach(System.out::println);
-                });
-        return null;
+        return countKprimes(k, start, end, primes(0, end));
     }
 
     public static int puzzle(int s) {
-        return 0;
+        long[] primes = primes(0, s);
+        long[] one = countKprimes(1, 0, s, primes);
+        long[] three = countKprimes(3, 0, s, primes);
+        long[] seven = countKprimes(7, 0, s, primes);
+        List<long[]> combinations = new ArrayList<>();
+        for (long se : seven) {
+            for (long t : three) {
+                for (long o : one) {
+                    long[] combination = new long[3];
+                    combination[0] = o;
+                    combination[1] = t;
+                    combination[2] = se;
+                    combinations.add(combination);
+                }
+            }
+        }
+        return (int) combinations.stream()
+                .filter(cs -> cs[0] + cs[1] + cs[2] == s)
+                .peek(cs -> System.out.println("" + cs[0] + " " + cs[1] + " " + cs[2]))
+                .count();
     }
 
     private static boolean isPrime(long number) {
@@ -46,28 +50,33 @@ public class KPrimes {
                 .toArray();
     }
 
-    private static void primeFactors(long number, long[] primes, List<Long> factors) {
-        for (int i = 0; i < primes.length; ++i) {
-            long prime = primes[i];
-            if (prime >= number) break;
-            if (number % prime == 0) {
-                factors.add(prime);
-                primeFactors(number / prime, primes, factors);
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        long number = 9;
-
-        final long[] primes = primes(0, number);
-        Arrays.stream(primes).forEach(System.out::println);
-
-        System.out.println("--------------------");
-
-        List<Long> factors = new ArrayList<>();
-        KPrimes.primeFactors(number, primes, factors);
-
-        factors.forEach(System.out::println);
+    private static long[] countKprimes(int k, long start, long end, long[] primes) {
+        if (k == 1) return primes;
+        return LongStream
+                .range(start, end + 1)
+                .boxed()
+                .map(n -> {
+                    long result = n;
+                    List<Long> factors = new ArrayList<>();
+                    while (!isPrime(result)) {
+                        for (long p : primes) {
+                            if (p > result) break;
+                            if (result % p == 0) {
+                                factors.add(p);
+                                result = result / p;
+                                break;
+                            }
+                        }
+                    }
+                    if (result != n) {
+                        factors.add(result);
+                    }
+                    factors.add(0, n);
+                    return factors;
+                })
+                .filter(factors -> factors.size() == k + 1)
+                .map(l -> l.get(0))
+                .mapToLong(Long::new)
+                .toArray();
     }
 }
